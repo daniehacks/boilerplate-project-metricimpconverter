118a474cd6c6c719d300f19ad9a63171cfea3b39
Danie Roberson <danierob2465@gmail.com>
2023-02-16 20:22:34 -0600

    adding first red test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,11 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
-
+    suite('Input Assertions', function(){
+        test('#wholeNumber, #decimalNumber', function(){
+            aWholeNumber = Math.floor(Math.random() * 100) + 1;
+            answer = convertHandler.getType(aWholeNumber);
+            assertEqual(answer,"integer");
+        });
+    });
 });
